6896b32d9850186ad3d2fa6f48f142778fbad7f5
Josh Max <joshumax@hotmail.com>
2022-09-13 09:51:20 -0700

    Only allow one instance of WinHideEx to run at a given time

--- WinHideExGUI/Program.cs
+++ WinHideExGUI/Program.cs
@@ -1,16 +1,29 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
+using WinHideExGUI.Properties;
 
 namespace WinHideExGUI
 {
     static class Program
     {
+        // Our mutex
+        private static Mutex mutex = null;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            mutex = new Mutex(true, Resources.AppName, out bool createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("Another instance of the application is already running!");
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new CustomApplicationContext(new MainForm()));
